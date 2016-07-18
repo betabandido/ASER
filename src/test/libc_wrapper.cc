3ced20c1d1be55c699e9c23eb2c0cfe186887f3a
Victor Jimenez <betabandido@gmail.com>
2016-07-18 18:25:39 +0200

    Improved testing.

--- src/test/libc_wrapper.cc
+++ src/test/libc_wrapper.cc
@@ -10,5 +10,11 @@ TEST(libc_wrapper, error_if_equal) {
   EXPECT_THROW(error_if_equal(-1, -1, ""), std::runtime_error);
 }
 
+TEST(libc_wrapper, error_if_not_equal) {
+  using util::error_if_not_equal;
+  EXPECT_EQ(error_if_not_equal(0, 0, ""), 0);
+  EXPECT_THROW(error_if_not_equal(-1, 0, ""), std::runtime_error);
+}
+
 } // namespace
 
