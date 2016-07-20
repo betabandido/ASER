d61fa0c3346c117ca01e38d216e4d0ec542057d8
Victor Jimenez <betabandido@gmail.com>
2016-07-20 14:59:47 +0200

    Add documentation to callback_timer.

--- src/util/timer.h
+++ src/util/timer.h
@@ -10,18 +10,35 @@
 namespace aser {
 namespace util {
 
+/** This class implements a timer that calls a function once the timer expires.
+ *
+ * If the timer object is destroyed, the timer is canceled and the callback
+ * function will not be called.
+ *
+ * The timer uses a thread that blocks until either the timer expires or is
+ * canceled.
+ */
 class callback_timer {
 public:
   typedef std::function<void()> callback_func;
 
+  /** Constructor.
+   *
+   * @param duration Timer duration (in milliseconds).
+   * @param callback Callback function to call once the timer expires.
+   */
   callback_timer(
       std::chrono::milliseconds duration,
       callback_func callback);
 
+  /** Destructor. */
   ~callback_timer();
 
 private:
+  /** Timer duration. */
   std::chrono::milliseconds duration_;
+
+  /** Callback function to call. */
   callback_func callback_;
 
   bool canceled_ { false };
