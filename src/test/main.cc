d93e8e62371f6e5ad63e0d31339033eb89f23344
Victor Jimenez <betabandido@gmail.com>
2016-10-30 02:35:42 +0200

    Reduce amount of logging during testing.

--- src/test/main.cc
+++ src/test/main.cc
@@ -1,6 +1,17 @@
 #include <gtest/gtest.h>
 
+#define BOOST_LOG_DYN_LINK 1
+
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+
 int main(int argc, char** argv) {
+  namespace logging = boost::log;
+  logging::core::get()->set_filter(
+      logging::trivial::severity >= logging::trivial::info
+  );
+
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
